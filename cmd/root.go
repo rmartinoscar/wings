07d920462b5d706449ae502c0588a2d2688d0365
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-11-02 17:24:39 +0000

    wip

--- cmd/root.go
+++ cmd/root.go
@@ -68,6 +68,8 @@ var versionCommand = &cobra.Command{
 	},
 }
 
+const defaultDockerRoot = "/var/lib/docker"
+
 func Execute() {
 	if err := rootCommand.Execute(); err != nil {
 		log2.Fatalf("failed to execute command: %s", err)
@@ -109,6 +111,43 @@ func isDockerSnap() bool {
 	return strings.Contains(info.DockerRootDir, "/var/snap/docker")
 }
 
+func ensureCorrectDockerRootDirectory() {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("Unable to initialize Docker client: %s", err)
+	}
+
+	defer cli.Close() // Close the client when the function returns (should not be needed, but just to be safe)
+
+	info, err := cli.Info(context.Background())
+	if err != nil {
+		log.Fatalf("Unable to get Docker info: %s", err)
+	}
+
+	dockerRoot := info.DockerRootDir
+
+	if dockerRoot == defaultDockerRoot {
+		log.Debugf("Docker root is already the default (%s). No action needed.\n", defaultDockerRoot)
+		return
+	}
+
+	target, err := os.Readlink(defaultDockerRoot)
+	if err == nil && target == dockerRoot {
+		log.Debugf("Symlink already correct: %s -> %s\n", dockerRoot, target)
+		return
+	}
+
+	if err := os.RemoveAll(defaultDockerRoot); err != nil {
+		log.Fatalf("Failed to remove existing %s: %v", defaultDockerRoot, err)
+	}
+
+	if err := os.Symlink(dockerRoot, defaultDockerRoot); err != nil {
+		log.Fatalf("Failed to create symlink: %v", err)
+	}
+
+	log.Debugf("Created symlink: %s -> %s\n", defaultDockerRoot, dockerRoot)
+}
+
 func rootCmdRun(cmd *cobra.Command, _ []string) {
 	printLogo()
 	log.Debug("running in debug mode")
@@ -118,6 +157,8 @@ func rootCmdRun(cmd *cobra.Command, _ []string) {
 		log.Error("Docker Snap installation detected. Exiting...")
 		os.Exit(1)
 	}
+
+	ensureCorrectDockerRootDirectory()
 
 	if ok, _ := cmd.Flags().GetBool("ignore-certificate-errors"); ok {
 		log.Warn("running with --ignore-certificate-errors: TLS certificate host chains and name will not be verified")
